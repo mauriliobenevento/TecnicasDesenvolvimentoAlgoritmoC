7ebe8b7539257ee16bcc35f00a9072ddb8181ef9
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 18:24:42 -0300

    Push 007

---
+++ Algoritmo007/Algoritmo007/main.c
@@ -0,0 +1,17 @@
+//  Algoritmo007
+//
+// criando uma tabela com \n e \t em apenas uma linha com o printf
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("\tfruta\tqtde\tvalor\n\tbanana\t10\t\t2\n\tgoiaba\t5\t\t2\n\tabacaxi\t4\t\t6\n\tmanga\t3\t\t1\n");
+    getchar(); // caso esteja utiliando windows, mude para: system("pause");
+    
+}
