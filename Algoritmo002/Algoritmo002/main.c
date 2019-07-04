57ee7adf5779f62377ba654664580bc2c16e753c
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 16:20:57 -0300

    Push do algoritmo002 sem alterações.

---
+++ Algoritmo002/Algoritmo002/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Algoritmo002
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Meu nome: Maurilio Benevento");
+    printf("RGM: 182999000");
+    printf("Curso: CIC");
+    printf("Disciplina: Técnicas de Desenvolvimento de Algoritmo");
+    
+}
+
+
+
