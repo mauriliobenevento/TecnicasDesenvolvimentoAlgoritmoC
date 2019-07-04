079c915bcebef6ee0d0e64b6c2410f953db23db8
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 19:06:23 -0300

    Push 15

---
+++ Algoritmo015/Algoritmo015/main.c
@@ -0,0 +1,24 @@
+//  Algoritmo015
+//
+//  Incremento com multiplicação
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    int x=2;
+    printf("Valor de X....: %d", x);
+    
+    x *=2;
+    x *=2;
+    x *=2;
+    x *=2;
+    
+    printf("\n\nValor de X incrementado....: %d\n\n", x);
+    
+    getchar();
+    
+}
