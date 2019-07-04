c2fa0e21d4be2688a75d2c36a7e0b66543f7c47b
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 20:57:01 -0300

    Push 37

---
+++ Algoritmo037/Algoritmo037/main.c
@@ -0,0 +1,17 @@
+//  Algoritmo037
+//
+//  Verificando no printf se a variável é maior ou menor, retornando
+//  se for maior 1, senão 0.
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int k=90;
+    printf("%i", k>91);
+    getchar();
+}
