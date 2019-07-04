105167d37c7c49e470413627394a14521ecac5a1
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 18:13:44 -0300

    Push 004

---
+++ Algoritmo004/Algoritmo004/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Algoritmo004
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Meu nome: Maurilio Benevento \n");
+    printf("RGM: 182999000 \n");
+    printf("Curso: CIC \n");
+    printf("Disciplina: Técnicas de Deenvolvimento de Algoritmo \n");
+    getchar();
+}
+
+// utilizando o \n para a quebra de linha. Compare com o algoritmo anterior. 
