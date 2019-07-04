abcfa3354fdb5d9689f4acc1e8235c8da13def97
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 16:44:06 -0300

    Commit algoritmo003

---
+++ Algoritmo003/Algoritmo003/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Algoritmo003
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Meu nome: Maurilio Benevento");
+    printf("\t RGM: 182999000");
+    printf("\t Curso: CIC");
+    printf("\t Disciplina: Técnicas de Desenvolvimento de Algoritmo");
+    
+    system("pause");
+    
+}
