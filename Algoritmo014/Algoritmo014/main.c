f3f0d34f83cf948ee5d82b3bd23210e1da2d6787
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 19:04:43 -0300

    Push 14

---
+++ Algoritmo014/Algoritmo014/main.c
@@ -0,0 +1,23 @@
+//  Algoritmo014
+//
+//  Decremento
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+int main()
+{
+    int x=0;
+    x = x+50;
+    printf("Valor de X....: %d", x);
+    x -=10;
+    x -=10;
+    x -=10;
+    x -=19;
+    printf("\n\nValor de X decrementado....: %d\n\n", x);
+    getchar();
+}
