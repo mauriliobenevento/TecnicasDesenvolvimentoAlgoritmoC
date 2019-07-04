24875a442233f8fdca351af1fba2907fcc2f2daf
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 20:18:07 -0300

    Push 029

---
+++ Algoritmo029/Algoritmo029/main.c
@@ -0,0 +1,39 @@
+//  Algoritmo029
+//
+//  Trabalhando com vetores.
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+
+ #include <stdio.h>
+ 
+ int main(void)
+ {
+ 
+     int vetor[10];
+ 
+ vetor[0] = 10;
+ vetor[1] = 11;
+ vetor[2] = 22;
+ vetor[3] = 33;
+ vetor[4] = 44;
+ vetor[5] = 55;
+ vetor[6] = 66;
+ vetor[7] = 77;
+ vetor[8] = 88;
+ vetor[9] = 99;
+ 
+ printf("Vetor 0....: %i",   vetor[0]);
+ printf("\nVetor 1....: %i", vetor[1]);
+ printf("\nVetor 2....: %i", vetor[2]);
+ printf("\nVetor 3....: %i", vetor[3]);
+ printf("\nVetor 4....: %i", vetor[4]);
+ printf("\nVetor 5....: %i", vetor[5]);
+ printf("\nVetor 6....: %i", vetor[6]);
+ printf("\nVetor 7....: %i", vetor[7]);
+ printf("\nVetor 8....: %i", vetor[8]);
+ printf("\nVetor 9....: %i", vetor[9]);
+ getchar();
+ }
