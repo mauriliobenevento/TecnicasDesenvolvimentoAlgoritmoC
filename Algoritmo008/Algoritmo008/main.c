6fe03143cf9747fe12fdaeb01b133c123cd6aade
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 18:31:41 -0300

    Update

--- Algoritmo008/Algoritmo008/main.c
+++ Algoritmo008/Algoritmo008/main.c
@@ -15,12 +15,10 @@ int main()
     int anonasc=1987;
     int anoatual=2019;
     
-    
     soma=val1+val2+val3;
     
     printf("O valor total é: %d\n",soma);
-    printf("A idade é: %d\n",anoatual-anonasc
-           );
+    printf("A idade é: %d\n",anoatual-anonasc);
     
     getchar(); // caso esteja utilizando windows, mude para: system("pause");
 }
