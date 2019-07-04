f36c0f0078b613837f867f696e50155401db1c8a
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 18:19:20 -0300

    Push 006

---
+++ Algoritmo006/Algoritmo006/main.c
@@ -0,0 +1,22 @@
+//  Algoritmo006
+//
+// criando uma tabela com \n e \t.
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\n\n\tfruta\t\tqtde\tvalor\n\n");
+    printf("\tbanana\t\t10\t\t1,00\n\n");
+    printf("\tgoiaba\t\t5\t\t4,2\n\n");
+    printf("\tabacaxi\t\t4\t\t3,3\n\n");
+    printf("\tmelao\t\t2\t\t4,1\n\n");
+    getchar();
+    
+}
+
+
