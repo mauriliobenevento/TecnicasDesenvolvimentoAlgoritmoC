a4ed9f9d26a5ca375ce6974ea6a4b01be642b9bb
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 16:13:50 -0300

    Commit do algoritmo001 na versão inicial sem as alterações comuns que eu faço.

---
+++ Algoritmo001/Algoritmo001/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algoritmo001
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
