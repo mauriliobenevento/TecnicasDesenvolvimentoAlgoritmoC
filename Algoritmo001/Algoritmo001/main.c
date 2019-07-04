b7567db6f5540a6af41e11736e07588c016afcc0
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 16:16:05 -0300

    Agora com as linhas comentadas para facilitar o entendimento do iniciante.

--- Algoritmo001/Algoritmo001/main.c
+++ Algoritmo001/Algoritmo001/main.c
@@ -6,10 +6,15 @@
 //  Copyright © 2019 Maurilio Benevento. All rights reserved.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+#include <stdio.h>         // aqui é declaração da biblioteca básica de entrada e saída para as funções em linguagem C
+
+int main()                 // abertura da função princial da linguagem C requerida pelo compilador.
+
+{                          // abertura de chave para identificar o corpo do algoritmo
+    
+    printf("Minha primeira linha em linguagem C"); // mostra um texto na tela / monitor
+    
+    
+}                          // fechamento de chave para indicar o fim do programa.
+
