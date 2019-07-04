9b659ec0610cba2b9946ddae79930df7d3f9835d
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 18:16:55 -0300

    Push 005

---
+++ Algoritmo005/Algoritmo005/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Algoritmo005
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Professor\t\tMaurilio\t\tBenevento\t\t\n");
+    getchar();
+    
+    
+}
+
+// utilização do \t (tabulação) e o \n (quebra de linha) - compare com o algoritmo anterior.
