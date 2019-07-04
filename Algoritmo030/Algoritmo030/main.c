5f2b6ae24e17dbbf0db42b14bfcc5838383150e3
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 20:22:08 -0300

    Push 030

---
+++ Algoritmo030/Algoritmo030/main.c
@@ -0,0 +1,30 @@
+//  Algoritmo030
+//
+//  Trabalhando com vetores outro exemplo
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    char s[80];
+    
+    s[0] = 'a';
+    s[2] = 'b';
+    s[3] = 'c';
+    s[4] = 'd';
+    s[5] = 'e';
+    s[6] = 'f';
+    printf("Vetor 0....: %c",   s[0]);
+    printf("\nVetor 2....: %c", s[2]);
+    printf("\nVetor 3....: %c", s[3]);
+    printf("\nVetor 4....: %c", s[4]);
+    printf("\nVetor 5....: %c", s[5]);
+    printf("\nVetor 6....: %c", s[6]);
+    printf("\nVetor 7....: %c", s[7]);
+    
+    getchar();
+}
