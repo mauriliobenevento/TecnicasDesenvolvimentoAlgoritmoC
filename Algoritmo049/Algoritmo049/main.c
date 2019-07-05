9cc9eb0f7fee1a6d489051dba42603e06662479b
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 21:49:22 -0300

    Push 049

---
+++ Algoritmo049/Algoritmo049/main.c
@@ -0,0 +1,19 @@
+//  Algoritmo049
+//
+//  Tabuada do 1 
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+ #include <stdio.h>
+ 
+ int main(void)
+ {
+ int i=1, num1=1;
+ 
+ for(i=1; i<=10; i++){
+ printf("%d X %d = %d\n", num1, i, num1*i);
+ }
+ getchar();
+ }
