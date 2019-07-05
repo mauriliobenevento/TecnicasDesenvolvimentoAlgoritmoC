a3a35036b4e0d877aef033371227de5ab018aac7
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 21:09:33 -0300

    Push 40

---
+++ Algoritmo040/Algoritmo040/main.c
@@ -0,0 +1,22 @@
+//  Algoritmo040
+//
+//  Atibuindo valor para mais varmem de uma vez
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x,y,z;
+    
+    x=y=z=90;
+    
+    printf("Mem X.....: %d\n", x);
+    printf("Mem Y.....: %d\n", y);
+    printf("Mem Z.....: %d", z);
+    
+    getchar();
+}
