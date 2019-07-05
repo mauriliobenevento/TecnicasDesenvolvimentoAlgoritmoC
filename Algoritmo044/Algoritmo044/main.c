6e90c34d74395b2dc98d980d024c4b25b8627370
mauriliobenevento <maurilio.benevento@gmail.com>
2019-07-04 21:23:35 -0300

    Push 044

---
+++ Algoritmo044/Algoritmo044/main.c
@@ -0,0 +1,22 @@
+//  Algoritmo044
+//
+//  Encadeando laços (condicionais)
+//  for (num=10; num>=0; num--)   // 2 em 2 // decrescente
+//
+//  Created by Maurilio Benevento on 04/07/19.
+//  Copyright © 2019 Maurilio Benevento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int laco_principal, laco1;
+    
+    for (laco_principal=1; laco_principal<=2; laco_principal++){
+        printf("\n### Primeiro Laço...: %d ### ",laco_principal);
+        for (laco1=10; laco1>=0; laco1--)
+            printf("\n@@@ Laço Interno...: %d @@@",laco1);
+    }
+    getchar();
+}
